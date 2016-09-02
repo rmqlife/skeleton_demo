49179b0f3fbd0b4ad637f3723aa6d83dc8c529de
rmqlife <rmqlife@dhcp-v880-5a-00789.1Xwireless.unc.edu>
2016-09-02 16:47:50 -0400

    extract max_circle function

--- read.m
+++ read.m
@@ -19,20 +19,7 @@ for times=1:10
     % select a point p on S
     i = randi(size(sset,1),1);
     p = sset(i,:);
-
-    % find the distance from p to B
-    % stack p
-    pstack = repmat(p,size(bset,1),1);
-
-    % distances, find the nearest 2 points
-    pdist = sqrt(sum((pstack-bset).^2,2));
-    [sorted, index] = sort(pdist,'ascend');
-
-    % plot
     plot(p(2),p(1),'gx');
-    for i = 1:1
-        bnear = bset(index(i),:);
-        %plot(bnear(2),bnear(1),'ro');
-        viscircles([p(2),p(1)], sorted(i),'Color','r','LineWidth',0.5);
-    end
+    [r, pmin] = max_circle(p,bset);
+    viscircles([p(2),p(1)], r, 'Color','r','LineWidth',0.5);
 end

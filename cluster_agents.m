cdd5944234f5a28407a998e50429ef25309cf1e8
Rmqlife <rmqlife@gmail.com>
2016-09-05 21:06:14 -0400

    cluster data export

---
+++ cluster_agents.m
@@ -0,0 +1,1 @@
+function clusters = cluster_agents();
--- read.m
+++ read.m
@@ -36,6 +36,7 @@ while num<=count
     num = num + 1;
 end
 
+
 for i = 1:size(agents_position,1)
     viscircles([agents_position(i,2),agents_position(i,1)], agent_radius, 'Color','w','LineWidth',0.5);
 end
@@ -58,7 +59,9 @@ for i = 1:size(agents_position,1)
     sa_match(:,i) = pdist <= cluster_radius;
 end
 
+clusters = cell(0);
 match_count = 0; 
+
 while match_count<size(agents_position,1)
     match = sum(sa_match,2);
     count = max(match);
@@ -79,7 +82,9 @@ while match_count<size(agents_position,1)
     r = cluster_radius(min_candidate);
     plot(p(2),p(1),'gx')
     viscircles([p(2),p(1)],r, 'Color','g','LineWidth',0.5);
-    
+    matched_agents_id = find(sa_match(min_candidate,:) == 1);
+    matched_agents_position = agents_position(matched_agents_id,:);
+    clusters=[clusters;{p,r,matched_agents_position}];
     % clear the matched agents
     for j=1:size(sa_match,2)
         if sa_match(min_candidate,j) == 1

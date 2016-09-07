470f194117cc1b026899ac0db43bf02f67df59ce
rmqlife <rmqlife@dhcp-v880-13-00331.1Xwireless.unc.edu>
2016-09-07 13:07:18 -0400

    add some cases and fix method

--- main.m
+++ main.m
@@ -20,16 +20,17 @@ agents_position = init_agents(im,20,agent_radius);
 clusters = cluster_agents(im,skeleton_im,agents_position,agent_radius);
 
 % cluster_struction(im,clusters(1,:),agent_radius);
-cluster_graph = cluster_connection(skeleton_im, clusters);
+cluster_graph = cluster_connection_fix(skeleton_im, clusters);
 
 figure, imshow(rgb), hold
 for i=1:size(clusters,1)
     [p,r,agents] = clusters{i,:};
-    for j=1:size(agents,1)
-        rectangle('Position',[agents(j,2)-agent_radius,agents(j,1)-agent_radius,agent_radius*2,agent_radius*2],'Curvature', [1 1], 'FaceColor','r') 
-    end
+%     % show each agents
+%     for j=1:size(agents,1)
+%         rectangle('Position',[agents(j,2)-agent_radius,agents(j,1)-agent_radius,agent_radius*2,agent_radius*2],'Curvature', [1 1], 'FaceColor','r') 
+%     end
     plot(p(2),p(1),'gx')
-    viscircles([p(2),p(1)],r, 'Color','g','LineWidth',0.5);
+    viscircles([p(2),p(1)],r, 'Color','w','LineWidth',0.5);
 end
 
 % draw graph
@@ -39,7 +40,7 @@ for i=1:size(clusters,1)
         if cluster_graph(i,j)>0
             pi = clusters{i,1};
             pj = clusters{j,1};
-            plot([pi(2),pj(2)]',[pi(1),pj(1)]','b-','LineWidth',3);
+            plot([pi(2),pj(2)]',[pi(1),pj(1)]','-','LineWidth',3);
         end
     end
 end
